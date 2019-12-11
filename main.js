aac058744dfd548d800aae61b023e53bde24d9f8
Andrew Zhou <andersonzhoutree@gmail.com>
2019-12-11 11:33:35 -0800

    finished css on destination.js class; fixed bugs and added images, side-container, and functional button in destination.js

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ function intializeApp() {
   var taco = new Taco();
   taco.addClickHandler();
 
-  var destinationDrop = new Destination();
-  destinationDrop.destinationsRender();
-
   //var test = new Transportation(3);
   //test.sendApiRequest(test.rockets);
   var introModal = new Modal();
